41bd6fdffaef8e5770681ac2e5fca21a3dc755dc
stranger2626 <stranger2626@gmail.com>
2016-09-30 04:09:47 +0300

    changed to a simpler version with a stream

--- Server.js
+++ Server.js
@@ -1,37 +1,50 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+fs = require('fs');
+http = require('http');
+url = require('url');
+path = require('path');
+port = process.argv[2]||8080;
 
-http.createServer(function(request, response) {
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-  
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
+
+http.createServer(function(req, res){
+  var fullFilePath = path.join(process.cwd(), url.parse(req.url).pathname);
+    fs.stat(fullFilePath, function(err, stats) {
+      if(err) {
+       res.statusCode = 404;
+       res.end("File not found");
+       return;
+     }
+     if (fs.statSync(fullFilePath).isDirectory()) {
+       var body = '<html>'+
+      '<head>'+
+      '<meta http-equiv="Content-Type" '+
+      'content="text/html; charset=UTF-8" />'+
+      '</head>'+
+      '<body>'+
+      '<a href="file.bin" class="button" download>Download binary file</a>' +
+      '</body>'+
+      '</html>';
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.write(body);
+      res.end();
+    }
+    else{
+      var file = new fs.ReadStream(fullFilePath);
+      sendFile(file,res);
     }
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
+});
 }).listen(parseInt(port, 10));
 
+    function sendFile(file,res){
+      file.pipe(res);
+      file.on('error',function(err){
+        res.statusCode=500;
+        res.end("Server Error");
+        console.error(err);
+        res.on('close',function(){
+          file.destroy();
+        });
+      });
+}
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
